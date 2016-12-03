496145d43dc2423d654ab76683a4db97894afb20
Dylan McKay <dylanmckay34@gmail.com>
2016-12-04 12:53:11 +1300

    Clean up the basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,13 +2,16 @@ extern crate gcast;
 
 use std::time::Duration;
 
+/// Connect to a Cast device on a given IP.
+/// Launch YouTube and receive status updates.
+///
+/// Usage:
+///
+/// cargo run --example basic 192.168.1.102
 fn main() {
-    let device_info = gcast::DeviceInfo {
-        ip_addr: "192.168.1.102".parse().unwrap(),
-        uuid: "d7288042-190b-5974-aa3b-2558f1cb0c0e".parse().unwrap(),
-    };
-
     let mut io = gcast::back::net::Io::new().unwrap();
+
+    let device_info = self::device_info();
     let mut device = gcast::Device::connect(device_info, &mut io).unwrap();
 
     /// Launch the YouTube app.
@@ -35,4 +38,26 @@ fn main() {
     }
 
     println!("Cast device disconnected");
+}
+
+fn device_info() -> gcast::DeviceInfo {
+    let ip_addr = match std::env::args().nth(1) {
+        Some(ip_addr) => match ip_addr.parse() {
+            Ok(ip_addr) => ip_addr,
+            Err(e) => {
+                println!("invalid IP address: {}", e);
+                std::process::exit(1);
+            },
+        },
+        None => {
+            println!("no IP address given");
+            println!("usage: basic 192.168.1.102");;;;
+            std::process::exit(1);
+        },
+    };
+
+    gcast::DeviceInfo {
+        ip_addr: ip_addr,
+        uuid: "d7288042-190b-5974-aa3b-2558f1cb0c0e".parse().unwrap(),
+    }
 }
